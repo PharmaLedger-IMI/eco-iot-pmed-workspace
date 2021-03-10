868b54ab806fd9f1f5ffb12cfd858980f3555fba
Nikos Liappas <nliappas@lst.tfo.upm.es>
2021-03-10 09:47:26 +0100

    sample pages

---
+++ patient-ssapp/code/scripts/controllers/MyDataController.js
@@ -0,0 +1,74 @@
+import ContainerController from "../../cardinal/controllers/base-controllers/ContainerController.js";
+
+
+const healthdatamodel = {
+
+    mydata: {
+
+        label: "Select your data to present:",
+        required: true,
+        options: [{
+            label: "All records",
+            value: 'All records'
+        },
+            {
+                label: "Allergies",
+                value: "Allergies"
+            },
+            {
+                label: "Clinical vitals",
+                value: "cv",
+                checked: true
+            },
+            {
+                label: "Conditions",
+                value: "Conditions"
+            },
+            {
+                label: "Immunizations",
+                value: "Immunizations"
+            },
+            {
+                label: "Lab results",
+                value: "Lab results"
+            },
+            {
+                label: "Medications/Treatments",
+                value: "Medications/Treatments"
+            }
+        ],
+        value: ''
+    }
+}
+
+
+export default class FormComponentExampleController extends ContainerController {
+    constructor(element) {
+        super(element);
+
+        this.model = this.setModel(JSON.parse(JSON.stringify(healthdatamodel)));
+
+        this.feedbackEmitter = null;
+
+        this.on('openFeedback', (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            this.feedbackEmitter = e.detail;
+        });
+
+        let radioSubmit = () => {
+            let choice = this.model.mydata.value;
+            if (choice === "cv"){
+                console.log(`Hello there, Clinical vitals!`,"radio Example","alert-primary")
+            } else {
+                console.log(`Good day to you, rest of choices!`,"radio Example","alert-primary")
+            }
+        }
+
+        this.on("View",radioSubmit,true);
+    }
+
+
+
+
+  }
