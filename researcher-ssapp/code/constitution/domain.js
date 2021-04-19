b8fc4116ba8bb210e567fe63e73ea299e39cb9ae
Rasel Ahmed <raselahmed.lucse@gmail.com>
2021-04-19 14:27:33 +0100

    Finalized design for patient status page

--- researcher-ssapp/code/constitution/domain.js
+++ researcher-ssapp/code/constitution/domain.js
@@ -1,6 +1,6 @@
 domainRequire=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"./../../domain":[function(require,module,exports){
 //Add specific code here (swarms, flows, assets, transactions)
-},{}],"/Users/raselahmed/Desktop/TVSWork/iot-pmed-workspace/researcher-ssapp/builds/tmp/domain_intermediar.js":[function(require,module,exports){
+},{}],"/Users/raselahmed/Desktop/TVSWork/openDSU/iot-pmed-workspace/researcher-ssapp/builds/tmp/domain_intermediar.js":[function(require,module,exports){
 (function (global){(function (){
 global.domainLoadModules = function(){ 
 
@@ -18,7 +18,7 @@ if (typeof $$ !== "undefined") {
 
 }).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 
-},{"./../../domain":"./../../domain"}]},{},["/Users/raselahmed/Desktop/TVSWork/iot-pmed-workspace/researcher-ssapp/builds/tmp/domain_intermediar.js"])
+},{"./../../domain":"./../../domain"}]},{},["/Users/raselahmed/Desktop/TVSWork/openDSU/iot-pmed-workspace/researcher-ssapp/builds/tmp/domain_intermediar.js"])
                     ;(function(global) {
                         global.bundlePaths = {"domain":"code/constitution/domain.js"};
                     })(typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {});
