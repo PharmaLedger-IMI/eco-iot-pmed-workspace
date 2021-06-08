bca6054ea3fab901a4b6bbea3c108d241d7b4e3f
Nikos Liappas <nliappas@lst.tfo.upm.es>
2021-06-08 17:38:56 +0200

    fixed template

---
+++ professional-ssapp/code/scripts/controllers/TrialManagementController.js
@@ -0,0 +1,20 @@
+const {WebcController} = WebCardinal.controllers;
+
+
+export default class TrialManagementController extends WebcController {
+    constructor(...props) {
+        super(...props);
+
+        this._attachHandlerGoBack();
+    }
+
+    _attachHandlerGoBack() {
+        this.on('go-back', (event) => {
+            console.log("Go Back button pressed");
+            this.navigateToPageTag('home');
+        });
+    }
+
+
+
+}
