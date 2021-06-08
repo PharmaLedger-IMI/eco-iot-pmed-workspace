2ef691378fdd979672859e1f5f27a3f955b707fc
victorgerardodominguez <victor.gerardo.dominguez@gmail.com>
2021-06-08 17:25:09 +0200

    prepare personalizedfeedback

--- patient-ssapp/code/scripts/controllers/PersonalizedFeedbackSampleController.js
+++ patient-ssapp/code/scripts/controllers/PersonalizedFeedbackSampleController.js
@@ -5,10 +5,17 @@ export default class PersonalizedFeedbackSampleController extends WebcController
     constructor(element, history) {
         super(element, history);
 
-       
+        this._attachHandlerGoBack();
       
 
 
+    }
+
+    _attachHandlerGoBack(){
+        this.on('go-back', (event) => {
+            console.log ("Go Back button pressed");
+            this.navigateToPageTag('home');
+        });
     }
 
 
