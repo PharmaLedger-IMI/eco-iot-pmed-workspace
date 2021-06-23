51b9c746ac52f511603d73e5aa5a048c89709d4b
Nikos Liappas <nliappas@lst.tfo.upm.es>
2021-06-23 15:21:16 +0200

    HL7 models

---
+++ utilitaries/HL7models/OrganizationModel.js
@@ -0,0 +1,34 @@
+export const organizationModelHL7 = {
+    // HL7 Organization Model //
+    OrganizationId: {
+        name: 'Organization System id',
+        label: "organization_system_id",
+        value: ''
+    },
+    OrganizationIdentifier: {
+        name: 'Organization Identifier',
+        label: "organization_identifier",
+        value: ''
+    },
+    OrganizationActive: {
+        name: 'Organization Active',
+        label: "organization_active",
+        value: ''
+    },
+    OrganizationType: {
+        name: 'Organization Type',
+        label: "organization_type",
+        value: ''
+    },
+    OrganizationName: {
+        name: 'Organization Name',
+        label: "organization_name",
+        value: ''
+    },
+    OrganizationPartOf: {
+        name: 'Organization Part Of',
+        label: "organization_part_of",
+        value: ''
+    },
+    
+};
---
+++ utilitaries/HL7models/RelatedPersonModel.js
@@ -0,0 +1,58 @@
+export const relatedPersonModelHL7 = {
+    // HL7 RelatedPerson Model //
+    RelatedPersonId: {
+        name: 'RelatedPerson System id',
+        label: "relatedPerson_system_id",
+        value: ''
+    },
+    RelatedPersonIdentifier: {
+        name: 'RelatedPerson Identifier',
+        label: "relatedPerson_identifier",
+        value: ''
+    },
+    RelatedPersonActive: {
+        name: 'RelatedPerson Active',
+        label: "relatedPerson_active",
+        value: ''
+    },
+    RelatedPersonPatient: {
+        name: 'RelatedPerson Patient',
+        label: "relatedPerson_patient",
+        value: ''
+    },
+    RelatedPersonRelationship: {
+        name: 'RelatedPerson Relationship',
+        label: "relatedPerson_relationship",
+        value: ''
+    },
+    RelatedPersonName: {
+        name: 'RelatedPerson Name',
+        label: "relatedPerson_name",
+        value: ''
+    },
+    RelatedPersonGender: {
+        name: 'RelatedPersonGender',
+        label: "relatedPerson_gender",
+        value: ''
+    },
+    RelatedPersonPeriod: {
+        name: 'RelatedPerson Period',
+        label: "relatedPerson_period",
+        value: ''
+    },
+    RelatedPersonCommunication: {
+        name: 'RelatedPerson Communication',
+        label: "relatedPerson_communication",
+        value: ''
+    },
+    RelatedPersonCommunicationLanguage: {
+        name: 'RelatedPerson Communication Language',
+        label: "relatedPerson_communication_language",
+        value: ''
+    },
+    RelatedPersonCommunicationPreferred: {
+        name: 'RelatedPerson Communication Preferred',
+        label: "relatedPerson_communication_prefered",
+        value: ''
+    }
+};
