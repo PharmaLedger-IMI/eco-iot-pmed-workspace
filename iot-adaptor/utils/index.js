d5ee51aa3af2465f6881f8c0dcf4768883ed7f2e
Rasel Ahmed <raselahmed.lucse@gmail.com>
2021-07-04 22:03:59 +0100

    updated config file for iot-adapter

--- iot-adaptor/utils/index.js
+++ iot-adaptor/utils/index.js
@@ -1,6 +1,6 @@
 const getClusterDomainConfig = (domain) => {
     const config = require("../../privatesky/modules/apihub/config");
-    return config.getConfig('endpointsConfig', 'iot-adaptor', 'domainStrategies', domain);
+    return config.getConfig('componentsConfig', 'iot-adaptor', 'domainStrategies', domain);
 };
 
 module.exports = {getClusterDomainConfig: getClusterDomainConfig}
