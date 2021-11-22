55b639f771b8ccd5fc0d45274e18aa72ef49dcbc
paiusCatalin <p.cata95@gmail.com>
2021-11-22 12:11:59 +0200

    [Refactor] [UI/UX] Update the Patient device history page of Professional SSApp using webcardinal features instead of old cardinal #84

--- professional-ssapp/code/scripts/controllers/PatientDeviceHistoryController.js
+++ professional-ssapp/code/scripts/controllers/PatientDeviceHistoryController.js
@@ -1,38 +1,16 @@
 const {WebcController} = WebCardinal.controllers;
 
-// const ViewPatientList = {
-//
-//     name: {
-//         name: 'NAME',
-//         label: "name",
-//         value: 'name'
-//     },
-//     id: {
-//         name: 'ID',
-//         label: "id",
-//         value: 'id'
-//     }
-// }
-
-
 export default class PatientDeviceHistoryController extends WebcController {
     constructor(element, history) {
+
         super(element, history);
-        //this.setModel(ViewPatientList);
-        this._attachHandler();
 
+        this.attachHandlerGoBack();
     }
 
-    _attachHandler(){
-        this.on('go-back', (event) => {
-            console.log ("Go Back button pressed");
-            this.navigateToPageTag('home');
+    attachHandlerGoBack() {
+        this.onTagClick('go-back', () => {
+            this.history.goBack();
         });
-
     }
-
-
-
-
-
 }
