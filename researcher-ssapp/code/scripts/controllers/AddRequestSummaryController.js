3dcc713d84e81e657c7de75a2b63c0a6e979ad33
Nikos Liappas <nliappas@lst.tfo.upm.es>
2021-11-22 16:41:21 +0100

    [Refactor] [UI/UX] Update Information Request

---
+++ researcher-ssapp/code/scripts/controllers/AddRequestSummaryController.js
@@ -0,0 +1,64 @@
+import InformationRequestService from "../services/InformationRequestService.js";
+import {contractModelHL7} from "../models/HL7/ContractModel.js";
+import CommunicationService from "../services/CommunicationService.js";
+const {WebcController} = WebCardinal.controllers;
+
+
+export default class AddRequestSummaryController extends WebcController {
+    constructor(...props) {
+        super(...props);
+
+        this.model = this.getState();
+
+        this.attachHandlerEditButton();
+        this.attachHandlerAcceptButton();
+    }
+
+    attachHandlerEditButton() {
+        this.onTagClick('request:edit', () => {
+            console.log("Edit button pressed");
+            this.navigateToPageTag('information-request', this.model.toObject());
+        });
+    }
+
+    sendMessageToPatient(operation, ssi) {
+        this.CommunicationService.sendMessage(CommunicationService.identities.IOT.PATIENT_IDENTITY, {
+            operation: operation,
+            ssi: ssi
+        });
+    }
+
+    attachHandlerAcceptButton() {
+        this.onTagClick('request:accept', () => {
+
+            //prepare contract // need to change later // use correct models
+            let initContract = JSON.parse(JSON.stringify(contractModelHL7));
+            initContract.ContractTitle = this.model.title;
+            initContract.ContractStatus = this.model.status;
+            initContract.ContractTerm = this.model.terms;
+            initContract.ContractIssued = new Date();
+            initContract.ContractVersion = 0;
+            initContract.ContractApplies = [this.model.startdate, this.model.enddate];
+
+            this.InformationRequestService = new InformationRequestService(this.DSUStorage);
+            this.InformationRequestService.saveInformationRequest(initContract, (err, data) => {
+                if (err) {
+                    this.navigateToPageTag('confirmation-page', {
+                        confirmationMessage: "An error has been occurred!",
+                        redirectPage: "home"
+                    });
+                    return console.log(err);
+                }
+                this.CommunicationService = CommunicationService.getInstance(CommunicationService.identities.IOT.RESEARCHER_IDENTITY);
+                this.sendMessageToPatient('information-request-response', data.uid);
+
+                this.model.dsuStatus = " Debug information: DSU KeySSI: ".concat('', data.KeySSI.substr(data.KeySSI.length - 10));
+
+                this.navigateToPageTag('confirmation-page', {
+                    confirmationMessage: "Request has been issued!",
+                    redirectPage: "home"
+                });
+            });
+        });
+    }
+}
--- researcher-ssapp/code/scripts/controllers/RequestsController.js
+++ researcher-ssapp/code/scripts/controllers/RequestsController.js
@@ -1,111 +1,25 @@
 const {WebcController} = WebCardinal.controllers;
 
 
-const requestInformationViewModel = {
-    Title: {
-        name: 'Title',
-        placeholder: 'Title',
-        required: true,
-        readOnly: false,
-        value: ''
-    },
-    Status: {
-        label: "Status",
-        placeholder: "Select the status",
-        required: true,
-        options: [
-            {
-                label: "Cancelled",
-                value: "cancelled"
-            },
-            {
-                label: "Executable",
-                value: "executable"
-            },
-            {
-                label: "Executed",
-                value: "executed"
-            },
-            {
-                label: "Offered",
-                value: "offered"
-            },
-        ]
-    },
-    Terms: {
-        label: "Terms",
-        placeholder: "Select the terms",
-        required: true,
-        options: [
-            {
-                label: "ECG",
-                value: "ecg"
-            },
-            {
-                label: "Respiration",
-                value: "respiration"
-            },
-            {
-                label: "SpO2",
-                value: "spo2"
-            },
-            {
-                label: "Temperature",
-                value: "temperature"
-            },
-        ]
-    },
-    requestsButton: {
-        label: "New request",
-        editState: false,
-    },
-    goBack: {
-        label: "Back",
-        editState: false
-    },
-    startDate: {
-        label: "Start Date",
-        input: {
-            name: "date-to-start",
-            required: false,
-            value: ''
-        }
-    },
-    endDate: {
-        label: "End Date",
-        input: {
-            name: "date-to-end",
-            required: false,
-            value: ''
-        }
-    },
-}
-
-
 export default class RequestsController extends WebcController {
     constructor(...props) {
 
         super(...props);
+
+        const prevState = this.getState() || {};
+        this.model = this.getRequestViewModel(prevState);
+
         this._attachHandlerIssueNewRequest()
         this._attachHandlerGoBack()
-        this.model = requestInformationViewModel;
+
 
     }
 
     _attachHandlerIssueNewRequest(){
         this.onTagClick('new:request', (event) => {
             console.log ("New information request button pressed");
-
-            let RequestState = {
-                title: this.model.Title.value,
-                startDate: this.model.startDate.value,
-                endDate: this.model.endDate.value,
-                status: this.model.Status.value,
-                terms: this.model.Terms.value
-            }
-            console.log(RequestState)
-
-            this.navigateToPageTag('issue-new-request', RequestState);
+            const requestData = this.prepareRequestData();
+            this.navigateToPageTag("add-request-summary", requestData);
         });
     }
 
@@ -114,6 +28,90 @@ export default class RequestsController extends WebcController {
             console.log ("Go back button pressed");
             this.navigateToPageTag('home');
         });
+    }
+
+    prepareRequestData() {
+        return {
+            title: this.model.title.value,
+            status: this.model.status.value,
+            terms: this.model.terms.value,
+            startdate: this.model.startdate.value,
+            enddate: this.model.enddate.value
+        };
+    }
+
+    getRequestViewModel(prevState) {
+        return {
+            title: {
+                name: 'title',
+                id: 'title',
+                label: "Title of the request",
+                placeholder: 'Request for oxygen data between June-August 2021',
+                required: true,
+                value: prevState.title || ""
+            },
+            startdate: {
+                name: 'Start Date',
+                id: 'Start Date',
+                label: "Starting date",
+                placeholder: 'Starting date',
+                value: prevState.startdate || ""
+            },
+            enddate: {
+                name: 'End Date',
+                id: 'End Date',
+                label: "Ending date",
+                placeholder: 'Ending date',
+                value: prevState.enddate || ""
+            },
+            // https://www.hl7.org/fhir/valueset-contract-status.html Revise it!
+            status: {
+                label: "Request Status",
+                required: true,
+                options: [
+                    {
+                        label: "Cancelled",
+                        value: 'Cancelled'
+                    },
+                    {
+                        label: "Executable",
+                        value: 'Executable'
+                    },
+                    {
+                        label: "Executed",
+                        value: 'Executed'
+                    },
+                    {
+                        label: "Offered",
+                        value: 'Offered'
+                    }
+                ],
+                value: prevState.status || ""
+            },
+            terms: {
+                label: "Terms",
+                required: true,
+                options: [
+                    {
+                        label: "ECG",
+                        value: "ECG"
+                    },
+                    {
+                        label: "Respiration",
+                        value: "Respiration"
+                    },
+                    {
+                        label: "SpO2",
+                        value: "SpO2"
+                    },
+                    {
+                        label: "Temperature",
+                        value: "Temperature"
+                    },
+                ],
+                value: prevState.terms || ""
+            }
+        }
     }
 
 }
