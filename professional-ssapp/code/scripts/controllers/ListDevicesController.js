a98deef34ae8afde5c19bb694eb3b2a51c4503eb
paiusCatalin <p.cata95@gmail.com>
2021-11-23 10:24:21 +0200

    Moved list device list responsibility to the right controller

--- professional-ssapp/code/scripts/controllers/ListDevicesController.js
+++ professional-ssapp/code/scripts/controllers/ListDevicesController.js
@@ -1,15 +1,37 @@
 const {WebcController} = WebCardinal.controllers;
 
+import DeviceService from "../services/DeviceService.js"
+
 export default class ListDevicesController extends WebcController {
     constructor(element, history) {
         super(element, history);
 
         this.model = {allDevice: []};
-        this.model.allDevices = JSON.parse(JSON.stringify(this.getState()));
+        this.DeviceService = new DeviceService();
 
+        this.attachModelHandlers();
         this.attachHandlerGoBack();
         this.attachHandlerViewDevice();
         this.attachHandlerEditDevice();
+
+        this.init();
+    }
+
+    init() {
+        this.DeviceService.searchDevice((err, devices) => {
+            if (err) {
+                return console.error(err);
+            }
+
+            this.model.allDevice = devices;
+        });
+    }
+
+    attachModelHandlers() {
+        this.model.addExpression(
+            'deviceListNotEmpty',
+            () => this.model.allDevice && this.model.allDevice.length > 0,
+            'participants');
     }
 
     attachHandlerGoBack() {
--- professional-ssapp/code/scripts/controllers/ManageDevicesController.js
+++ professional-ssapp/code/scripts/controllers/ManageDevicesController.js
@@ -1,41 +1,31 @@
 const {WebcController} = WebCardinal.controllers;
 
-import DeviceService from "../services/DeviceService.js"
-
 export default class ManageDevicesController extends WebcController {
     constructor(element, history) {
         super(element, history);
         this.model = {};
 
-        this._attachHandlerAddDevice();
-        this._attachHandlerSearchDevice();
-        this._attachHandlerGoBack();
+        this.attachHandlerAddDevice();
+        this.attachHandlerSearchDevice();
+        this.attachHandlerGoBack();
 
     }
 
-    _attachHandlerAddDevice() {
+    attachHandlerAddDevice() {
         this.onTagClick('devices:add', () => {
             this.navigateToPageTag('add-device');
         });
     }
 
-    _attachHandlerSearchDevice() {
+    attachHandlerSearchDevice() {
         this.onTagClick('devices:search', () => {
-            this.DeviceService = new DeviceService();
-            this.DeviceService.searchDevice((err, devices) => {
-                if (err) {
-                    return console.log(err);
-                }
-                this.navigateToPageTag('list-all-devices', devices);
-            });
+            this.navigateToPageTag('list-all-devices');
         });
     }
 
-    _attachHandlerGoBack() {
+    attachHandlerGoBack() {
         this.onTagClick('devices:back', () => {
             this.navigateToPageTag('home');
         });
     }
-
-
 }
