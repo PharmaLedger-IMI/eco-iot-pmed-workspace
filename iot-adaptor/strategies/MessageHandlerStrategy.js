856d6b29c97b1e8ad8920ae7dd1de001dad18915
nliappas <67898132+nliappas@users.noreply.github.com>
2022-03-28 16:59:20 +0200

    add device message

--- iot-adaptor/strategies/MessageHandlerStrategy.js
+++ iot-adaptor/strategies/MessageHandlerStrategy.js
@@ -1,5 +1,5 @@
 const commonServices = require("common-services")
-const {EvidenceService, CommunicationService} = commonServices;
+const {EvidenceService, CommunicationService, DeviceServices} = commonServices;
 
 module.exports = async function (err, message) {
 
@@ -11,10 +11,15 @@ module.exports = async function (err, message) {
     message = JSON.parse(message);
 
     const evidenceService = new EvidenceService();
+    const deviceService = new DeviceServices();
 
     switch (message.operation) {
         case "new_evidence":
+            console.log(message);
             evidenceService.mount(message.ssi, (err, mountedEntity) => {
+                if (err){
+                    console.log(err);
+                }
                 const entityId = mountedEntity.uid;
                 evidenceService.getEvidence(entityId, (err, evidence) => {
                     evidence.title = "The updated title from IOT Adaptor";
@@ -22,6 +27,23 @@ module.exports = async function (err, message) {
                         console.log("Evidence updated");
                     });
                 })
+            });
+            break;
+        case "add_device":
+            console.log(message);
+            deviceService.mountDevice(message.sReadSSI, (err, mountedDevice) => {
+                if (err){
+                    console.log(err);
+                }
+                const entityId = mountedDevice.sReadSSI;
+                console.log(mountedDevice)
+
+
+                // Push it to the hospital database or the iot Adaptor wallet
+
+
+
+
             });
             break;
         default:
