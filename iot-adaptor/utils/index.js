cabd977f7cb21674b172353fbf289cb71817f78d
paiusCatalin <paiusCatalin>
2022-05-17 09:36:11 +0300

    moved iot-adapter to separate repository | cleanup

--- iot-adaptor/utils/index.js
+++
@@ -1,6 +0,0 @@
-const getClusterDomainConfig = (domain) => {
-    const config = require("../../privatesky/modules/apihub/config");
-    return config.getConfig('componentsConfig', 'iot-adaptor', 'domainStrategies', domain);
-};
-
-module.exports = {getClusterDomainConfig: getClusterDomainConfig}
