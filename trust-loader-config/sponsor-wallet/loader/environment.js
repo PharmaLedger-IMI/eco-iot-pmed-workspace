31f2a50abfc5eb09404f12a11baea6a5b2612ebd
mastaleru <mastaleru.rafael@gmail.com>
2022-05-20 12:23:04 +0300

    added sponsor-ssapp & sponsor-theme

---
+++ trust-loader-config/sponsor-wallet/loader/environment.js
@@ -0,0 +1,18 @@
+export default {
+    appName: "sponsor-wallet",
+    vault: "server",
+    agent: "browser",
+    system: "any",
+    browser: "any",
+    mode: "dev-secure",
+    domain: "iot",
+    didDomain: "iot",
+    vaultDomain: "vault",
+    enclaveType: "WalletDBEnclave",
+    sw: false,
+    pwa: false,
+    workspace: "iot",
+    iotAdaptorEndpoint: "http://localhost:4500",
+    "legenda for properties":
+        " vault:(server, browser) agent:(mobile, browser) system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)",
+};
