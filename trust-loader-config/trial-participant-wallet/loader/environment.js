e84c1e7ea86bdec517d7983c2d2c5cc9950eeb45
mastaleru <mastaleru.rafael@gmail.com>
2022-11-07 16:45:59 +0200

    added hl7 configuration usage for development

--- trust-loader-config/trial-participant-wallet/loader/environment.js
+++ trust-loader-config/trial-participant-wallet/loader/environment.js
@@ -13,6 +13,7 @@ export default {
     sw: false,
     pwa: false,
     workspace: "iot",
+    hl7:false,
     iotAdaptorEndpoint: "http://localhost:4500",
     'legenda for properties':
         ' vault:(server, browser) agent:(mobile, browser) system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(development, release) sw:(true, false) pwa:(true, false)',
